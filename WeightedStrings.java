1e41682cba705c1fcac6e6f739a7eee0f8b9c001
samueltumewu <samueltumewu21@gmail.com>
2024-06-30 14:15:38 +0700

    add Weighted Strings code solution

---
+++ WeightedStrings.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class WeightedStrings {
+    public static void main(String[] args) {
+        /*
+            INPUT FIELDS
+         */
+        String input = "abbcccd";
+        int[] arrayQueries = {1,3,9,8};
+
+        /*
+            MAIN CODE
+         */
+        final int DECREMENTOR = 96;
+        input = input.toLowerCase(); //forced to convert into lower case
+
+        LinkedList<Integer> weightResult = new LinkedList<Integer>();
+        char beforeStateChar = '0';
+        int beforeStateCharCounter = 0;
+
+        for(char inputChar : input.toCharArray()) {
+            if (beforeStateChar != '0' && inputChar == beforeStateChar) {
+                beforeStateCharCounter++;
+                weightResult.add(((int) inputChar - DECREMENTOR) * (beforeStateCharCounter>0?beforeStateCharCounter:1));
+            } else {
+                weightResult.add((int) inputChar - DECREMENTOR);
+                beforeStateChar = inputChar;
+                beforeStateCharCounter = 1;
+            }
+        }
+
+        LinkedList<String> finalResult = new LinkedList<String>();
+        for(int query: arrayQueries) {
+            if(weightResult.contains(query)) {
+                finalResult.add("YES");
+            } else {
+                finalResult.add("NO");
+            }
+        }
+
+        System.out.println(finalResult);
+    }
+}
